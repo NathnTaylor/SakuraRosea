3bfae29d16978698255568e3d1018071667f99ee
Gareth Coles <gdude2002@gmail.com>
2020-07-09 00:06:09 +0100

    Ensure client-only stuff only happens on the client

--- src/main/java/rocks/sakira/sakurarosea/SakuraRosea.java
+++ src/main/java/rocks/sakira/sakurarosea/SakuraRosea.java
@@ -2,37 +2,24 @@ package rocks.sakira.sakurarosea;
 
 import com.google.common.collect.ImmutableSet;
 import net.minecraft.block.Block;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.Atlases;
-import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.RenderTypeLookup;
-import net.minecraft.client.renderer.model.Material;
-import net.minecraft.client.renderer.texture.AtlasTexture;
 import net.minecraft.tileentity.TileEntityType;
-import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.TextureStitchEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import rocks.sakira.sakurarosea.common.Materials;
 import rocks.sakira.sakurarosea.common.block.Blocks;
 import rocks.sakira.sakurarosea.common.entities.Entities;
-import rocks.sakira.sakurarosea.common.entities.renderers.SakuraBoatEntityRenderer;
 import rocks.sakira.sakurarosea.common.event.BlockClickedEventHandler;
 import rocks.sakira.sakurarosea.common.feature.Features;
 import rocks.sakira.sakurarosea.common.item.Items;
 import rocks.sakira.sakurarosea.common.tileentities.TileEntities;
-import rocks.sakira.sakurarosea.common.tileentities.renderers.SakuraChestTileEntityRenderer;
-import rocks.sakira.sakurarosea.common.tileentities.renderers.SakuraSignTileEntityRenderer;
-import rocks.sakira.sakurarosea.common.wood.FaeWoodType;
 import rocks.sakira.sakurarosea.common.world.biome.Biomes;
 
 import java.lang.reflect.Field;
@@ -44,10 +31,6 @@ import static rocks.sakira.sakurarosea.Constants.MOD_ID;
 @Mod(MOD_ID)
 public class SakuraRosea {
     private static final Logger LOGGER = LogManager.getLogger();
-
-    static {
-        Atlases.SIGN_MATERIALS.put(FaeWoodType.SAKURA, new Material(Atlases.SIGN_ATLAS, new ResourceLocation(MOD_ID, "entity/signs/sakura")));
-    }
 
     public SakuraRosea() {
         final IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
@@ -108,43 +91,14 @@ public class SakuraRosea {
     }
 
     @SubscribeEvent
+    @OnlyIn(Dist.CLIENT)
     public void clientSetup(final FMLClientSetupEvent event) {
-        RenderTypeLookup.setRenderLayer(Blocks.PINK_CLAY_DOOR_BLOCK.get(), RenderType.getCutout());
-        RenderTypeLookup.setRenderLayer(Blocks.SAKURA_SAPLING_BLOCK.get(), RenderType.getCutout());
-        RenderTypeLookup.setRenderLayer(Blocks.WHITE_CLAY_DOOR_BLOCK.get(), RenderType.getCutout());
-
-        ClientRegistry.bindTileEntityRenderer(
-                TileEntities.SAKURA_CHEST_ENTITY.get(),
-                SakuraChestTileEntityRenderer::new
-        );
-
-        ClientRegistry.bindTileEntityRenderer(
-                TileEntities.SAKURA_SIGN_ENTITY.get(),
-                SakuraSignTileEntityRenderer::new
-        );
-
-        Minecraft.getInstance().getRenderManager().register(
-                Entities.SAKURA_BOAT_ENTITY.get(),
-                new SakuraBoatEntityRenderer(Minecraft.getInstance().getRenderManager())
-        );
+        ClientOnlySetup.setup();
     }
 
-    @OnlyIn(Dist.CLIENT)
     @SubscribeEvent
+    @OnlyIn(Dist.CLIENT)
     public void textureStitchPre(final TextureStitchEvent.Pre event) {
-        if (event.getMap().getTextureLocation().equals(Atlases.CHEST_ATLAS)) {
-            event.addSprite(Materials.SAKURA_CHEST_MATERIAL.getTextureLocation());
-            event.addSprite(Materials.SAKURA_CHEST_LEFT_MATERIAL.getTextureLocation());
-            event.addSprite(Materials.SAKURA_CHEST_RIGHT_MATERIAL.getTextureLocation());
-        }
-
-        if (event.getMap().getTextureLocation().equals(Atlases.SIGN_ATLAS)) {
-            event.addSprite(Materials.SAKURA_SIGN_MATERIAL.getTextureLocation());
-        }
-
-        if (event.getMap().getTextureLocation().equals(AtlasTexture.LOCATION_BLOCKS_TEXTURE)) {
-            event.addSprite(Materials.SAKURA_SHIELD_BASE.getTextureLocation());
-            event.addSprite(Materials.SAKURA_SHIELD_NO_PATTERN.getTextureLocation());
-        }
+        ClientOnlySetup.textureStitchPre(event);
     }
 }
