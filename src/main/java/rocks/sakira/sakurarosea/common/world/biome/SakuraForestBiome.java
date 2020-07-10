abd861871cd8f1d6b87db1629b591dd089b80084
Gareth Coles <gdude2002@gmail.com>
2020-07-10 21:32:52 +0100

    Reformat biome classes a bit

--- src/main/java/rocks/sakira/sakurarosea/common/world/biome/SakuraForestBiome.java
+++ src/main/java/rocks/sakira/sakurarosea/common/world/biome/SakuraForestBiome.java
@@ -14,9 +14,23 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 
 public final class SakuraForestBiome extends Biome {
     public SakuraForestBiome() {
-        super((new Biome.Builder()).surfaceBuilder(SurfaceBuilder.DEFAULT, SurfaceBuilder.GRASS_DIRT_GRAVEL_CONFIG).precipitation(Biome.RainType.RAIN).category(Biome.Category.FOREST).depth(0.1F).scale(0.2F).temperature(0.7F).downfall(0.8F).waterColor(4159204).waterFogColor(329011).parent(null));
+        super(
+                new Biome.Builder()
+                        .surfaceBuilder(SurfaceBuilder.DEFAULT, SurfaceBuilder.GRASS_DIRT_GRAVEL_CONFIG)
+                        .precipitation(Biome.RainType.RAIN)
+                        .category(Biome.Category.FOREST)
+                        .depth(0.1F)
+                        .scale(0.2F)
+                        .temperature(0.7F)
+                        .downfall(0.8F)
+                        .waterColor(4159204)
+                        .waterFogColor(329011)
+                        .parent(null)
+        );
+
         this.addStructure(Feature.MINESHAFT.withConfiguration(new MineshaftConfig(0.004D, MineshaftStructure.Type.NORMAL)));
         this.addStructure(Feature.STRONGHOLD.withConfiguration(IFeatureConfig.NO_FEATURE_CONFIG));
+
         DefaultBiomeFeatures.addCarvers(this);
         DefaultBiomeFeatures.addStructures(this);
         DefaultBiomeFeatures.addLakes(this);
@@ -30,6 +44,7 @@ public final class SakuraForestBiome extends Biome {
         DefaultBiomeFeatures.addSprings(this);
         DefaultBiomeFeatures.addFreezeTopLayer(this);
         DefaultBiomeFeatures.addGrass(this);
+
         this.addSpawn(EntityClassification.CREATURE, new Biome.SpawnListEntry(EntityType.SHEEP, 12, 4, 4));
         this.addSpawn(EntityClassification.CREATURE, new Biome.SpawnListEntry(EntityType.PIG, 10, 4, 4));
         this.addSpawn(EntityClassification.CREATURE, new Biome.SpawnListEntry(EntityType.CHICKEN, 10, 4, 4));
--- src/main/java/rocks/sakira/sakurarosea/common/world/biome/SakuraForestHillsBiome.java
+++ src/main/java/rocks/sakira/sakurarosea/common/world/biome/SakuraForestHillsBiome.java
@@ -15,9 +15,23 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 public final class SakuraForestHillsBiome extends Biome {
     public SakuraForestHillsBiome() {
 
-        super((new Biome.Builder()).surfaceBuilder(SurfaceBuilder.DEFAULT, SurfaceBuilder.GRASS_DIRT_GRAVEL_CONFIG).precipitation(Biome.RainType.RAIN).category(Biome.Category.FOREST).depth(0.45F).scale(0.3F).temperature(0.7F).downfall(0.8F).waterColor(4159204).waterFogColor(329011).parent(null));
+        super(
+                new Biome.Builder()
+                        .surfaceBuilder(SurfaceBuilder.DEFAULT, SurfaceBuilder.GRASS_DIRT_GRAVEL_CONFIG)
+                        .precipitation(Biome.RainType.RAIN)
+                        .category(Biome.Category.FOREST)
+                        .depth(0.45F)
+                        .scale(0.3F)
+                        .temperature(0.7F)
+                        .downfall(0.8F)
+                        .waterColor(4159204)
+                        .waterFogColor(329011)
+                        .parent(null)
+        );
+
         this.addStructure(Feature.MINESHAFT.withConfiguration(new MineshaftConfig(0.004D, MineshaftStructure.Type.NORMAL)));
         this.addStructure(Feature.STRONGHOLD.withConfiguration(IFeatureConfig.NO_FEATURE_CONFIG));
+
         DefaultBiomeFeatures.addCarvers(this);
         DefaultBiomeFeatures.addStructures(this);
         DefaultBiomeFeatures.addLakes(this);
@@ -29,6 +43,7 @@ public final class SakuraForestHillsBiome extends Biome {
         DefaultBiomeFeatures.addMushrooms(this);
         DefaultBiomeFeatures.addReedsAndPumpkins(this);
         DefaultBiomeFeatures.addSprings(this);
+
         DefaultBiomeFeatures.addFreezeTopLayer(this);
         this.addSpawn(EntityClassification.CREATURE, new Biome.SpawnListEntry(EntityType.SHEEP, 12, 4, 4));
         this.addSpawn(EntityClassification.CREATURE, new Biome.SpawnListEntry(EntityType.PIG, 10, 4, 4));
